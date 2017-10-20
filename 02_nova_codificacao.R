c568b5923da2b83dbb4364657d81da40d5caddfa
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-10-20 21:54:50 +0200

    update tables and correct contra e afavor

--- 02_nova_codificacao.R
+++ 02_nova_codificacao.R
@@ -50,6 +50,12 @@ head(emo_obj)
 head(molten_ocasiao)
 
 # CRUZA EMOÇÕES E ARGUMENTOS
+molten$argumento = factor(molten$argumento,
+                          levels = c('1C','2C','3C','4C','5C','6C',
+                                     '7C','8C','9C','10C','11C','12C',
+                                     '1F','2F','3F','4F','5F','6F',
+                                     '7F','8F','9F','10F','11F','12F'))
+
 table(molten$emocao, molten$argumento)
 xtable::xtable(table(molten$emocao, molten$argumento))
 # testa
@@ -59,12 +65,13 @@ chisq.test(table(molten$emocao, molten$argumento))
 
 
 # PREPARANDO
-index = grep("C", molten$argumento)
+indexc = grep("C", molten$argumento)
+indexf = grep("F", molten$argumento)
 molten$argumento = as.character(molten$argumento)
-molten$posicionamento[index] = "Contra"
-molten$posicionamento[-index] = "A Favor"
-contra = molten[index,]
-afavor = molten[-index,]
+molten$posicionamento[indexc] = "Contra"
+molten$posicionamento[indexf] = "A Favor"
+contra = molten[indexc,]
+afavor = molten[indexf,]
 head(contra)
 head(afavor)
 
@@ -75,12 +82,21 @@ chisq.test(table(molten$posicionamento, molten$emocao))
 fisher.test(table(molten$posicionamento, molten$emocao))
 
 # CRUZA EMOÇÕES E ARGUMENTOS
-xtable::xtable(table(contra$emocao, contra$argumento))
+contra$argumento = factor(contra$argumento,
+                             levels = c('1C','2C','3C','4C','5C','6C',
+                                        '7C','8C','9C','10C','11C','12C'))
+
+#levels(contra$argumento) = c('1C','2C','3C','4C','5C','6C',
+#                             '7C','8C','9C','10C','11C','12C')
 table(contra$emocao, contra$argumento)
+xtable::xtable(table(contra$emocao, contra$argumento))
 chisq.test(table(contra$emocao, contra$argumento))
 fisher.test(table(contra$emocao, contra$argumento), simulate.p.value = T, B=5000)
 
 
+afavor$argumento = factor(afavor$argumento,
+                          levels = c('1F','2F','3F','4F','5F','6F',
+                                     '7F','8F','9F','10F','11F','12F'))
 xtable::xtable(table(afavor$emocao, afavor$argumento))
 table(afavor$emocao, afavor$argumento)
 fisher.test(table(afavor$emocao, afavor$argumento), simulate.p.value = T, B=5000)
